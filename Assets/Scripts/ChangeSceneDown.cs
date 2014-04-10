83d2a04eefd6d5d2d0c27bce2a12ba7cbdaba5c6
theQure <rasmus_kure@hotmail.com>
2014-04-10 15:03:53 +0200

    Debug changes

--- Assets/Scripts/ChangeSceneDown.cs
+++ Assets/Scripts/ChangeSceneDown.cs
@@ -5,6 +5,12 @@ public class ChangeSceneDown : MonoBehaviour {
 
 	void OnMouseUpAsButton()
 	{
+		Debug.Log(Application.loadedLevel);
+
+		if (Application.loadedLevel == 2)
+			Application.LoadLevel(1);
 		Application.LoadLevel (Application.loadedLevel - 1);
+
+
 	}
 }
