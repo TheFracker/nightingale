5d4ab2d5d6724091f7c49bb6f424fb49d0e901dc
Anders Olsen <TheFracker@gmail.com>
2014-04-24 17:14:28 +0200

    Scripts for scene3 now done

--- Assets/Scripts/Scene3/Crowd.cs
+++ Assets/Scripts/Scene3/Crowd.cs
@@ -1,15 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Crowd : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Crowd : MessageBehaviour {
 	
+	int click = 0;
+	
+	void OnMouseUpAsButton()
+	{
+		Messenger.SendToListeners(new ClickMessage(gameObject, "Scene3ClickMessage", "crowd", ++click));
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
