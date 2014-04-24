1197b0109aa2de0407567ea88cb2875644de1ea8
Anders Olsen <TheFracker@gmail.com>
2014-04-24 17:23:15 +0200

    Scripts for scene7 done

--- Assets/Scripts/Scene7/Curtains.cs
+++ Assets/Scripts/Scene7/Curtains.cs
@@ -1,15 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Curtains : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Curtains : MessageBehaviour {
 	
+	int click = 0;
+	
+	void OnMouseUpAsButton()
+	{
+		Messenger.SendToListeners(new ClickMessage(gameObject, "Scene7ClickMessage", "curtains", ++click));
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
