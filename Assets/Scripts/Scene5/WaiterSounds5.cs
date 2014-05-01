23399bd52382ef5cbb2703bbcff16f09b8777a9e
Anders Olsen <TheFracker@gmail.com>
2014-05-01 14:21:24 +0200

    Added sounds to scene5

---
+++ Assets/Scripts/Scene5/WaiterSounds5.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaiterSounds5 : MonoBehaviour {
+
+	AudioSource waiter1;
+	AudioSource waiter2;
+	AudioSource[] waiterSounds = new AudioSource[2];
+	int lastWaiterSound = 0;
+	
+	void Start () {
+		waiter1 = gameObject.AddComponent ("AudioSource") as AudioSource;
+		waiter2 = gameObject.AddComponent ("AudioSource") as AudioSource;
+		
+		AudioClip ac1 = Resources.Load("Non-Iconic/Scene5/Long servant-01") as AudioClip;
+		AudioClip ac2 = Resources.Load("Non-Iconic/Scene5/Long servant-02") as AudioClip;
+		
+		waiter1.clip = ac1;
+		waiter2.clip = ac2;
+		
+		waiterSounds [0] = waiter1;
+		waiterSounds [1] = waiter2;
+	}
+	
+	void OnMouseUpAsButton()
+	{
+		waiterSounds [lastWaiterSound].Stop ();
+		int waiter = Random.Range (0, 2);
+		waiterSounds [waiter].Play ();
+		lastWaiterSound = waiter;
+	}
+}
