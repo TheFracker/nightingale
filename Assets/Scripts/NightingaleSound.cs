8d5043823d29da8dc94dcae31c08b85903d07c12
Anders Olsen <TheFracker@gmail.com>
2014-05-01 11:47:30 +0200

    Removed the old sounds, and added a lot of new ones. Sounds for scene1 should be done

--- Assets/Scripts/NightingaleSound.cs
+++ Assets/Scripts/NightingaleSound.cs
@@ -6,7 +6,8 @@ public class NightingaleSound : MonoBehaviour {
 	AudioSource tweet1;
 	AudioSource tweet2;
 	AudioSource tweet3;
-	AudioSource[] tweets = new AudioSource[3];
+	AudioSource tweet4;
+	AudioSource[] tweets = new AudioSource[4];
 	int lastTweet = 0;
 
 	void Start () 
@@ -14,18 +15,22 @@ public class NightingaleSound : MonoBehaviour {
 		tweet1 = gameObject.AddComponent ("AudioSource") as AudioSource;
 		tweet2 = gameObject.AddComponent ("AudioSource") as AudioSource;
 		tweet3 = gameObject.AddComponent ("AudioSource") as AudioSource;
+		tweet4 = gameObject.AddComponent ("AudioSource") as AudioSource;
 
-		AudioClip ac1 = Resources.Load("Nightingale1") as AudioClip;
-		AudioClip ac2 = Resources.Load("Nightingale2") as AudioClip;
-		AudioClip ac3 = Resources.Load("Nightingale3") as AudioClip;
+		AudioClip ac1 = Resources.Load("Non-Iconic/Nightingale1") as AudioClip;
+		AudioClip ac2 = Resources.Load("Non-Iconic/Nightingale2") as AudioClip;
+		AudioClip ac3 = Resources.Load("Non-Iconic/Nightingale3") as AudioClip;
+		AudioClip ac4 = Resources.Load("Non-Iconic/Nightingale4") as AudioClip;
 
 		tweet1.clip = ac1;
 		tweet2.clip = ac2;
 		tweet3.clip = ac3;
+		tweet3.clip = ac4;
 
 		tweets [0] = tweet1;
 		tweets [1] = tweet2;
 		tweets [2] = tweet3;
+		tweets [3] = tweet4;
 	}
 
 	void OnMouseUpAsButton()
---
+++ Assets/Scripts/Scene1/FrogSounds.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogSounds : MonoBehaviour {
+
+	AudioSource ribbit1;
+	AudioSource ribbit2;
+	AudioSource ribbit3;
+	AudioSource[] ribbits = new AudioSource[3];
+	int lastRibbit = 0;
+	
+	void Start () 
+	{
+		ribbit1 = gameObject.AddComponent ("AudioSource") as AudioSource;
+		ribbit2 = gameObject.AddComponent ("AudioSource") as AudioSource;
+		ribbit3 = gameObject.AddComponent ("AudioSource") as AudioSource;
+		
+		AudioClip ac1 = Resources.Load("Non-Iconic/Scene1/Frog_1") as AudioClip;
+		AudioClip ac2 = Resources.Load("Non-Iconic/Scene1/Frog_2") as AudioClip;
+		AudioClip ac3 = Resources.Load("Non-Iconic/Scene1/Frog_3") as AudioClip;
+		
+		ribbit1.clip = ac1;
+		ribbit2.clip = ac2;
+		ribbit3.clip = ac3;
+		
+		ribbits [0] = ribbit1;
+		ribbits [1] = ribbit2;
+		ribbits [2] = ribbit3;
+	}
+	
+	void OnMouseUpAsButton()
+	{
+		ribbits [lastRibbit].Stop ();
+		int ribbit = Random.Range (0, 3);
+		ribbits [ribbit].Play ();
+		lastRibbit = ribbit;
+	}
+}
